ce6d67a50929ef28ab20cf76b0ac356be033913f
VinuthaSadasivam <125736671+VinuthaSadasivam@users.noreply.github.com>
2024-06-19 13:32:58 +0700

    Update CheckOut.js
    fetch request changed from "http://IP address/" to "https://vinuarts.com"

--- src/CheckOut/CheckOut.js
+++ src/CheckOut/CheckOut.js
@@ -47,7 +47,7 @@ const Checkout = () => {
         })),
       };
 
-      const response = await fetch("http://93.127.194.16:1337/api/orders", {
+      const response = await fetch("https://vinuarts.com/api/orders", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(requestBody),
